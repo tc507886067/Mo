39a98ecfcc0d3199036e3d7cbaf5c51b183cdaf7
XIANG <108182531+tc507886067@users.noreply.github.com>
2023-11-14 21:50:14 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,8 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/page?sid=cb7629daa529e73209c85dc4ad7f6165&uid=14053084&pid=ce5858ae11d84ecf539913e7b6e62b16&tid=21e1ceeab47dfde085811ca45d193031&view_time=1699243324&bind_user=0&create_user=0&tid=21e1ceeab47dfde085811ca45d193031&sid=cb7629daa529e73209c85dc4ad7f6165&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit-EZNAr0bVnRjT-tYvfkL1Q&token=0ad890f9648f334b24e33f3acd5530a645d831e8fd1448dcb2f21e4849e0d2f0&expired_time=2024-11-05T12:02:04%2008:00
-",
+    "https://www.maimemo.com/page?sid=e706df3271420551d5153c731ec388d6&uid=14053084&pid=ecb08be95727e206e629213092e90262&tid=e76038ca2dbe8d431d8d1554bf556d21&view_time=1699937958&bind_user=0&create_user=0&tid=e76038ca2dbe8d431d8d1554bf556d21&sid=e706df3271420551d5153c731ec388d6&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit-EZNAr0bVnRjT-tYvfkL1Q&token=b12a658008baf42da24c40e1460694084ad535a9d9d843c7a4e718311e3dc7ff&expired_time=2024-11-13T12:59:18%2008:00",
   ]
   /**
    * 生成随机数字
